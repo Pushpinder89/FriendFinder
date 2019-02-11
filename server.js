36bff56302eb537955581af067b7418719f07c22
PKaur <dgninder@gmail.com>
2019-02-10 16:19:34 -0800

    trying style working on heroku

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // Dependencies
 // =============================================================
 var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
 
 
 // Sets up the Express App
@@ -11,6 +13,9 @@ var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("."));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 app.use('/static', express.static(path.join(__dirname, 'app/public')));
 
 ///////////////////////
