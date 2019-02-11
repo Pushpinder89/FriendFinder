4f6faf0f359d384421a89bcef707c52651c03c2a
PKaur <dgninder@gmail.com>
2019-02-10 16:15:33 -0800

    trying style working on heroku

--- server.js
+++ server.js
@@ -11,16 +11,16 @@ var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use(express.static("public"));
+app.use('/static', express.static(path.join(__dirname, 'app/public')));
 
 ///////////////////////
 //API ROUTES
 // =============================================================
-require("./app/controllers/apiRoutes")(app);
+require("./app/controllers/apiRoutes.js")(app);
 
 //HTML ROUTES
 // =============================================================
-require("./app/controllers/htmlRoutes")(app);
+require("./app/controllers/htmlRoutes.js")(app);
 
 // Starts the server to begin listening
 // =============================================================
